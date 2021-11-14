4c9a22c57bb7c317ffaac29d6a33c69026717bb4
Jengyoung <young_pallete@naver.com>
2021-11-14 21:04:36 +0900

    chore: project initialized settings
    1. typescript basic setting
    2. react basic setting
    3. eslint basic setting
    4. webpack customized setting

---
+++ webpack.config.ts
@@ -0,0 +1,48 @@
+import path from "path";
+import { Configuration, HotModuleReplacementPlugin } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const config: Configuration = {
+  mode: "development",
+  output: {
+    publicPath: "/",
+  },
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new HotModuleReplacementPlugin(),
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    historyApiFallback: true,
+    port: 3000,
+    open: true,
+    hot: true
+  },
+};
+
+export default config;

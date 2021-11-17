c0ae1d636352054bd8bda8d25a0c410ca58442ed
Jengyoung <young_pallete@naver.com>
2021-11-18 08:16:22 +0900

    fix: Storybook Emotion@11 version conflict
    원인: 웹팩 번들로 인한 오류
    해결: storybook에 웹팩에 관한 추가 설정으로 해결

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,18 @@
+const path = require('path');
+
+const resolvePath = (_path) => path.join(process.cwd(), _path);
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        '@emotion/styled': resolvePath('node_modules/@emotion/styled'),
+      },
+    },
+  }),
+};
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,41 +1,16 @@
+import styled from '@emotion/styled';
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from './Button';
+const Button = styled.button`
+  background: black;
+`;
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Example/Button',
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as ComponentMeta<typeof Button>;
-
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
-
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
+  argTypes: {},
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
+const Template = () => <Button>Test!</Button>;
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+export const Default = Template.bind({});

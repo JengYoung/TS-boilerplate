f3d86b2a749b828190461f6f3e78fcddd3bf98d8
Jengyoung <young_pallete@naver.com>
2021-11-20 22:23:38 +0900

    fix: storybook custom path import 되지 않는 현상 수정

--- .storybook/main.js
+++ .storybook/main.js
@@ -12,6 +12,13 @@ module.exports = {
       alias: {
         ...config.resolve.alias,
         '@emotion/styled': resolvePath('node_modules/@emotion/styled'),
+        '@components': resolvePath('src/components'),
+        '@hooks': resolvePath('src/hooks'),
+        '@pages': resolvePath('src/pages'),
+        '@stories': resolvePath('src/stories'),
+        '@assets': resolvePath('src/assets'),
+        '@utils': resolvePath('src/utils'),
+        '@apis': resolvePath('src/apis'),
       },
     },
   }),
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,84 +1,82 @@
-import path from "path";
-import { Configuration, HotModuleReplacementPlugin } from "webpack";
+import path from 'path';
+import { Configuration, HotModuleReplacementPlugin } from 'webpack';
 
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-import ESLintPlugin from "eslint-webpack-plugin";
+import ESLintPlugin from 'eslint-webpack-plugin';
 import MiniCSSExtractPlugin from 'mini-css-extract-plugin';
-import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin"
-
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 
 const MODE_DEVELOPMENT = 'development';
-const MODE_PRODUCTION = 'production'
+const MODE_PRODUCTION = 'production';
 const isDevelopment = process.env.NODE_ENV === MODE_DEVELOPMENT;
 
 const config: Configuration = {
   mode: isDevelopment ? MODE_DEVELOPMENT : MODE_PRODUCTION,
   output: {
-    publicPath: "/",
+    publicPath: '/',
   },
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
   module: {
     rules: [
       {
         test: /\.[jt]sx?$/i,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
             presets: [
-              "@babel/preset-env",
-              "@babel/preset-react",
-              "@babel/preset-typescript",
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
             ],
-            plugins: [isDevelopment && require.resolve('react-refresh/babel')].filter(Boolean),
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel'),
+            ].filter(Boolean),
           },
         },
       },
       {
         test: /\.css?$/,
-        use: [...(isDevelopment ? ['style-loader'] : [MiniCSSExtractPlugin.loader] ), 'css-loader'],
+        use: [
+          ...(isDevelopment ? ['style-loader'] : [MiniCSSExtractPlugin.loader]),
+          'css-loader',
+        ],
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: './index.html',
     }),
     new ForkTsCheckerWebpackPlugin({
-      async: false
+      async: false,
     }),
     new ESLintPlugin({
-      extensions: ["tsx", "ts", "js"],
+      extensions: ['tsx', 'ts', 'js'],
     }),
-    ...(isDevelopment 
-      ? [
-        new HotModuleReplacementPlugin(),
-        new ReactRefreshWebpackPlugin()  
-      ] 
-      : [
-        new MiniCSSExtractPlugin()
-      ]
-    )
+    ...(isDevelopment
+      ? [new HotModuleReplacementPlugin(), new ReactRefreshWebpackPlugin()]
+      : [new MiniCSSExtractPlugin()]),
   ],
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
-    static: path.join(__dirname, "dist"),
+    static: path.join(__dirname, 'dist'),
     historyApiFallback: true,
     port: 3000,
     compress: true,
     open: true,
-    hot: true
+    hot: true,
   },
   optimization: {
     splitChunks: {
       chunks: 'all',
     },
     minimize: true,
-  }
+  },
 };
 
 export default config;
